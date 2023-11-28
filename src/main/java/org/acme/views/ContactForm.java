bc59815d6ca09e0d532716720dd41bc9f8aaef8d
Matti Tahvonen <matti@vaadin.com>
2023-11-28 16:16:39 +0200

    Basic functionality

---
+++ src/main/java/org/acme/views/ContactForm.java
@@ -0,0 +1,121 @@
+package org.acme.views;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.shared.Registration;
+import org.acme.data.entity.Company;
+import org.acme.data.entity.Contact;
+import org.acme.data.entity.Status;
+
+import java.util.List;
+
+public class ContactForm extends FormLayout {
+  TextField firstName = new TextField("First name");
+  TextField lastName = new TextField("Last name");
+  EmailField email = new EmailField("Email");
+  ComboBox<Status> status = new ComboBox<>("Status");
+  ComboBox<Company> company = new ComboBox<>("Company");
+
+  Button save = new Button("Save");
+  Button delete = new Button("Delete");
+  Button close = new Button("Cancel");
+  // Other fields omitted
+  BeanValidationBinder<Contact> binder = new BeanValidationBinder<>(Contact.class);
+  public ContactForm(List<Company> companies, List<Status> statuses) {
+    addClassName("contact-form");
+
+    company.setItems(companies);
+    company.setItemLabelGenerator(Company::getName);
+    status.setItems(statuses);
+    status.setItemLabelGenerator(Status::getName);
+
+    binder.bindInstanceFields(this);
+
+    add(firstName, 
+        lastName,
+        email,
+        company,
+        status,
+        createButtonsLayout());
+  }
+
+  private HorizontalLayout createButtonsLayout() {
+    save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+    delete.addThemeVariants(ButtonVariant.LUMO_ERROR);
+    close.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+
+    save.addClickShortcut(Key.ENTER);
+    close.addClickShortcut(Key.ESCAPE);
+
+    save.addClickListener(event -> validateAndSave());
+    delete.addClickListener(event -> fireEvent(new DeleteEvent(this, binder.getBean())));
+    close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+
+    binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+
+    return new HorizontalLayout(save, delete, close); 
+  }
+
+  private void validateAndSave() {
+    if(binder.isValid()) {
+      fireEvent(new SaveEvent(this, binder.getBean()));
+    }
+  }
+
+  public void setContact(Contact contact) {
+    binder.setBean(contact);
+  }
+
+  public static abstract class ContactFormEvent extends ComponentEvent<ContactForm> {
+    private Contact contact;
+
+    protected ContactFormEvent(ContactForm source, Contact contact) {
+      super(source, false);
+      this.contact = contact;
+    }
+
+    public Contact getContact() {
+      return contact;
+    }
+  }
+
+  public static class SaveEvent extends ContactFormEvent {
+    SaveEvent(ContactForm source, Contact contact) {
+      super(source, contact);
+    }
+  }
+
+  public static class DeleteEvent extends ContactFormEvent {
+    DeleteEvent(ContactForm source, Contact contact) {
+      super(source, contact);
+    }
+
+  }
+
+  public static class CloseEvent extends ContactFormEvent {
+    CloseEvent(ContactForm source) {
+      super(source, null);
+    }
+  }
+
+  public Registration addDeleteListener(ComponentEventListener<DeleteEvent> listener) {
+    return addListener(DeleteEvent.class, listener);
+  }
+
+  public Registration addSaveListener(ComponentEventListener<SaveEvent> listener) {
+    return addListener(SaveEvent.class, listener);
+  }
+  public Registration addCloseListener(ComponentEventListener<CloseEvent> listener) {
+    return addListener(CloseEvent.class, listener);
+  }
+
+}
--- src/main/java/org/acme/views/ListView.java
+++ src/main/java/org/acme/views/ListView.java
@@ -1,34 +1,118 @@
 package org.acme.views;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import org.acme.data.entity.Contact;
+import org.acme.data.service.CrmService;
 
-@PageTitle("list")
+import java.util.Collections;
+
 @Route(value = "")
+@PageTitle("Contacts | Vaadin CRM")
 public class ListView extends VerticalLayout {
+    private final CrmService service;
+    Grid<Contact> grid = new Grid<>(Contact.class);
+    TextField filterText = new TextField();
 
-    public ListView() {
-        setSpacing(false);
+    ContactForm form;
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+    public ListView(CrmService service) {
+        this.service = service;
+        addClassName("list-view");
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        configureGrid();
+        configureForm();
+
+        add(getToolbar(), getContent());
+        updateList();
+        closeEditor();
+    }
+
+    private void updateList() {
+        grid.setItems(service.findAllContacts(filterText.getValue()));
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, form);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, form);
+        content.addClassNames("content");
+        content.setSizeFull();
+        return content;
+    }
+
+    private void configureForm() {
+        form = new ContactForm(service.findAllCompanies(), service.findAllStatuses());        form.setWidth("25em");
+        form.addSaveListener(this::saveContact);
+        form.addDeleteListener(this::deleteContact);
+        form.addCloseListener(e -> closeEditor());
+    }
+
+    private void saveContact(ContactForm.SaveEvent event) {
+        service.saveContact(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void deleteContact(ContactForm.DeleteEvent event) {
+        service.deleteContact(event.getContact());
+        updateList();
+        closeEditor();
+    }
+
+    private void configureGrid() {
+        grid.addClassNames("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        grid.asSingleSelect().addValueChangeListener(event ->
+                editContact(event.getValue()));
+    }
+
+    private HorizontalLayout getToolbar() {
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
+
+        Button addContactButton = new Button("Add contact");
+        addContactButton.addClickListener(click -> addContact());
+
+        var toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
+    }
+
+
+    public void editContact(Contact contact) {
+        if (contact == null) {
+            closeEditor();
+        } else {
+            form.setContact(contact);
+            form.setVisible(true);
+            addClassName("editing");
+        }
+    }
+
+    private void closeEditor() {
+        form.setContact(null);
+        form.setVisible(false);
+        removeClassName("editing");
+    }
+
+    private void addContact() {
+        grid.asSingleSelect().clear();
+        editContact(new Contact());
     }
 
 }
